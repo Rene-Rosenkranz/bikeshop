da906f015c5290132db8f8677705957d4293f212
Ines Rodrigues <ines.rodrigues@hotmail.de>
2023-07-17 16:15:04 +0200

    Button bei Fertigungsaufträgen schon mal vorläufig angepasst

--- src/pages/simulation/components/ProductionProgram.jsx
+++ src/pages/simulation/components/ProductionProgram.jsx
@@ -11,11 +11,11 @@ import {
   Button,
   Typography,
   Popover,
+  Box,
 } from "@mui/material";
-import { Box } from "@mui/system";
 import { useTranslation } from "react-i18next";
 import { useGlobalState } from "../../../components/GlobalStateProvider";
-import { InfoOutlined, Label } from "@mui/icons-material";
+import { InfoOutlined } from "@mui/icons-material";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -30,6 +30,8 @@ function ProductionProgram(props) {
   const [iInputValue, setInputValue] = useState(0);
   const [oCurrentElement, setCurrentElement] = useState({});
   const [expandedItemIndex, setExpandedItemIndex] = useState(null);
+  const [allInformationOpen, setAllInformationOpen] = useState(false); // New state for all information open/close
+
   const allowedKeys = [
     "ArrowLeft",
     "ArrowRight",
@@ -84,10 +86,10 @@ function ProductionProgram(props) {
   const handleSequenceNumberChange = (itemId, newSequenceNumber) => {
     const newState = state;
     const otherObject = state["productionlist"].find(
-      (oObject) => oObject.sequenceNumer === newSequenceNumber
+        (oObject) => oObject.sequenceNumer === newSequenceNumber
     );
     const currentObject = state["productionlist"].find(
-      (oObject) => oObject.id === itemId
+        (oObject) => oObject.id === itemId
     );
     const iOldSequenceNumber = currentObject.sequenceNumer;
     const iCurrentIndex = state["productionlist"].indexOf(currentObject);
@@ -96,7 +98,7 @@ function ProductionProgram(props) {
     newState["productionlist"][iCurrentIndex].sequenceNumer = newSequenceNumber;
     newState["productionlist"][iOtherIndex].sequenceNumer = iOldSequenceNumber;
     newState["productionlist"].sort(
-      (a, b) => a.sequenceNumer - b.sequenceNumer
+        (a, b) => a.sequenceNumer - b.sequenceNumer
     );
     setItems(newState["productionlist"]);
     setState(newState);
@@ -110,226 +112,227 @@ function ProductionProgram(props) {
     }
   };
 
+  const handleToggleAllInformation = () => {
+    setAllInformationOpen(!allInformationOpen);
+  };
+
   return (
-    <>
-      <Box alignContent="center">
-        <Tooltip arrow title={t("simulation.tooltipProductionProgram")}>
-          <InfoOutlined />
-        </Tooltip>
-        {items.length > 0 ? (
-          items.map((oElement, index) => (
-            <Paper
-              key={oElement.sequenceNumer}
-              elevation={3}
-              sx={{
-                margin: "1rem",
-                padding: "1rem",
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  marginBottom: "1rem",
-                }}
-              >
-                <Box sx={{ display: "flex", alignItems: "center" }}>
-                  <InputLabel>{t("simulation.part")}</InputLabel>
-                  <Box sx={{ marginLeft: "0.5rem" }}>{oElement.article}</Box>
-                </Box>
-                <FormControl
-                  required={true}
-                  size="small"
-                  sx={{ display: "flex", alignItems: "center" }}
-                >
-                  <Input
-                    type="number"
-                    error={!bValid}
-                    inputProps={{
-                      min: 0,
-                      onKeyDown: (event) => {
-                        event.preventDefault();
-                      },
-                    }}
-                    aria-describedby="form-helper"
-                    value={oElement.quantity}
-                    sx={{ width: "6rem", textAlign: "center" }}
-                    onChange={(oEvent) => {
-                      const bIsEmpty = !(
-                        /^[0-9]*$/.test(oEvent.target.value) &&
-                        oEvent.target.value.length > 0
-                      );
-                      fValidHandler(!bIsEmpty);
-                      if (bIsEmpty) return;
-                      const oNewState = state;
-                      const oIndex = oNewState["productionlist"].find(
-                        (oObject) => oObject.article === oElement.article
-                      );
-                      const iIndex =
-                        oNewState["productionlist"].indexOf(oIndex);
-                      oNewState["productionlist"][iIndex].quantity =
-                        oEvent.target.valueAsNumber;
-                      setState(oNewState);
-                    }}
-                  />
-                  {bValid && (
-                    <FormHelperText id="form-helper">
-                      {t("simulation.productionAmount")}
-                    </FormHelperText>
-                  )}
-                  {!bValid && (
-                    <FormHelperText id="form-helper" error>
-                      {t("simulation.errorMissingInput")}
-                    </FormHelperText>
-                  )}
-                </FormControl>
-                <Box sx={{ marginLeft: "1rem" }}>
-                  <Button
-                    variant="outlined"
-                    onClick={(oEvent) => handleClick(oEvent, oElement)}
+      <>
+        <Box alignContent="center">
+          <Tooltip arrow title={t("simulation.tooltipProductionProgram")}>
+            <InfoOutlined />
+          </Tooltip>
+          {items.length > 0 ? (
+              items.map((oElement, index) => (
+                  <Paper
+                      key={oElement.sequenceNumer}
+                      elevation={3}
+                      sx={{
+                        margin: "1rem",
+                        padding: "1rem",
+                        display: "flex",
+                        flexDirection: "column",
+                      }}
                   >
-                    {t("simulation.splitItem")}
-                  </Button>
-                </Box>
-                <Box>
-                <InputLabel>{t("simulation.sequenceNumber")}</InputLabel>
-                <Select
-                    variant="outlined"
-                    value={oElement.sequenceNumer}
-                    onChange={(e) =>
-                        handleSequenceNumberChange(
-                            oElement.id,
-                            parseInt(e.target.value)
-                        )
-                    }
-                    sx={{ marginLeft: "10px", width: "6rem" }}
-                >
-                  {[...Array(state["productionlist"].length)].map(
-                      (_, index) => (
-                          <MenuItem key={index} value={index + 1}>
-                            {index + 1}
-                          </MenuItem>
-                      )
-                  )}
-                </Select>
-              </Box>
-          </Box>
-              {expandedItemIndex === index && (
-                <Box marginTop="1rem" sx={{ fontSize: "16px", textAlign: "left" }}>
-                  <p>
-                    {t("simulation.component")}: {oElement.name}
-                  </p>
-                  {oElement.informations.map((info, infoIndex) => (
-                    <p key={infoIndex}>{info}</p>
-                  ))}
-                </Box>
-              )}
-              <Box sx={{ display: "flex", justifyContent: "flex-start" }}>
-                <Box sx={{ marginTop: "1rem", marginLeft: "1rem" }}>
-                  <Button
-                    variant="contained"
-                    onClick={() => handleToggleOrderInfos(index)}
-                    sx={{
+                    <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "space-between",
+                          marginBottom: "1rem",
+                        }}
+                    >
+                      <Box sx={{ display: "flex", alignItems: "center" }}>
+                        <InputLabel>{t("simulation.part")}</InputLabel>
+                        <Box sx={{ marginLeft: "0.5rem" }}>{oElement.article}</Box>
+                      </Box>
+                      <FormControl
+                          required={true}
+                          size="small"
+                          sx={{ display: "flex", alignItems: "center" }}
+                      >
+                        <Input
+                            type="number"
+                            error={!bValid}
+                            inputProps={{
+                              min: 0,
+                              onKeyDown: (event) => {
+                                event.preventDefault();
+                              },
+                            }}
+                            aria-describedby="form-helper"
+                            value={oElement.quantity}
+                            sx={{ width: "6rem", textAlign: "center" }}
+                            onChange={(oEvent) => {
+                              const bIsEmpty = !(
+                                  /^[0-9]*$/.test(oEvent.target.value) &&
+                                  oEvent.target.value.length > 0
+                              );
+                              fValidHandler(!bIsEmpty);
+                              if (bIsEmpty) return;
+                              const oNewState = state;
+                              const oIndex = oNewState["productionlist"].find(
+                                  (oObject) => oObject.article === oElement.article
+                              );
+                              const iIndex =
+                                  oNewState["productionlist"].indexOf(oIndex);
+                              oNewState["productionlist"][iIndex].quantity =
+                                  oEvent.target.valueAsNumber;
+                              setState(oNewState);
+                            }}
+                        />
+                        {bValid && (
+                            <FormHelperText id="form-helper">
+                              {t("simulation.productionAmount")}
+                            </FormHelperText>
+                        )}
+                        {!bValid && (
+                            <FormHelperText id="form-helper" error>
+                              {t("simulation.errorMissingInput")}
+                            </FormHelperText>
+                        )}
+                      </FormControl>
+                      <Box sx={{ marginLeft: "1rem" }}>
+                        <Button
+                            variant="outlined"
+                            onClick={(oEvent) => handleClick(oEvent, oElement)}
+                        >
+                          {t("simulation.splitItem")}
+                        </Button>
+                      </Box>
+                      <Box>
+                        <InputLabel>{t("simulation.sequenceNumber")}</InputLabel>
+                        <Select
+                            variant="outlined"
+                            value={oElement.sequenceNumer}
+                            onChange={(e) =>
+                                handleSequenceNumberChange(
+                                    oElement.id,
+                                    parseInt(e.target.value)
+                                )
+                            }
+                            sx={{ marginLeft: "10px", width: "6rem" }}
+                        >
+                          {[...Array(state["productionlist"].length)].map(
+                              (_, index) => (
+                                  <MenuItem key={index} value={index + 1}>
+                                    {index + 1}
+                                  </MenuItem>
+                              )
+                          )}
+                        </Select>
+                      </Box>
+                    </Box>
+                    {(expandedItemIndex === index || allInformationOpen) && (
+                        <Box marginTop="1rem" sx={{ fontSize: "16px", textAlign: "left" }}>
+                          <p>
+                            {t("simulation.component")}: {oElement.name}
+                          </p>
+                          {oElement.informations.map((info, infoIndex) => (
+                              <p key={infoIndex}>{info}</p>
+                          ))}
+                        </Box>
+                    )}
+                  </Paper>
+              ))
+          ) : (
+              <Typography>{t("simulation.noItems")}</Typography>
+          )}
+        </Box>
+          <Box sx={{ display: "left", marginTop: "3rem" }}>
+              <Button
+                  variant="contained"
+                  onClick={handleToggleAllInformation}
+                  sx={{
                       backgroundColor: "white",
                       color: "black",
                       "&:hover": {
-                        backgroundColor: "crimsonblue",
+                          backgroundColor: "crimsonblue",
                       },
-                    }}
-                  >
-                    {expandedItemIndex === index
-                      ? "Informationen ausblenden"
-                      : "Informationen anzeigen"}
-                  </Button>
-                </Box>
-              </Box>
-              <Popover
-                id={id}
-                open={bOpen}
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "center",
-                }}
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "center",
-                }}
-              >
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    padding: "1rem",
                   }}
-                >
-                  <Typography variant="h6" sx={{ marginBottom: "1rem" }}>
-                    {t("simulation.splitItem")}
-                  </Typography>
-                  <Input
-                    id="input"
-                    type="number"
-                    sx={{
-                      width: "12rem",
-                      marginBottom: "1rem",
-                      padding: "0.5rem",
-                      fontSize: "1rem",
-                    }}
-                    value={iInputValue}
-                    onChange={(e) => setInputValue(Number(e.target.value))}
-                    inputProps={{
-                      min: 1,
-                      max: iMaxValue,
-                    }}
-                    onInput={(oEvent) => {
-                      if (oEvent.target.value > iMaxValue) {
-                        oEvent.target.value = iMaxValue;
-                      }
-                    }}
-                  />
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      width: "100%",
-                    }}
-                  >
-                    <Button
-                      variant="outlined"
-                      onClick={(oEvent) => handleSplitItem(oElement, oEvent)}
-                      sx={{
-                        margin: "0.5rem",
-                        fontSize: "0.875rem",
-                        fontWeight: "normal",
-                      }}
-                    >
-                      {t("simulation.confirm")}
-                    </Button>
-                    <Button
-                      variant="outlined"
-                      onClick={handleClose}
-                      sx={{
-                        margin: "0.5rem",
-                        fontSize: "0.875rem",
-                        fontWeight: "normal",
-                      }}
-                    >
-                      {t("simulation.cancel")}
-                    </Button>
-                  </Box>
-                </Box>
-              </Popover>
-            </Paper>
-          ))
-        ) : (
-          <Typography>{t("simulation.noItems")}</Typography>
-        )}
-      </Box>
-    </>
+              >
+                  {allInformationOpen ? "Informationen ausblenden" : "Informationen anzeigen"}
+              </Button>
+          </Box>
+
+        <Popover
+            id={id}
+            open={bOpen}
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+        >
+          <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                padding: "1rem",
+              }}
+          >
+            <Typography variant="h6" sx={{ marginBottom: "1rem" }}>
+              {t("simulation.splitItem")}
+            </Typography>
+            <Input
+                id="input"
+                type="number"
+                sx={{
+                  width: "12rem",
+                  marginBottom: "1rem",
+                  padding: "0.5rem",
+                  fontSize: "1rem",
+                }}
+                value={iInputValue}
+                onChange={(e) => setInputValue(Number(e.target.value))}
+                inputProps={{
+                  min: 1,
+                  max: iMaxValue,
+                }}
+                onInput={(oEvent) => {
+                  if (oEvent.target.value > iMaxValue) {
+                    oEvent.target.value = iMaxValue;
+                  }
+                }}
+            />
+            <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+            >
+              <Button
+                  variant="outlined"
+                  onClick={(oEvent) => handleSplitItem(oElement, oEvent)}
+                  sx={{
+                    margin: "0.5rem",
+                    fontSize: "0.875rem",
+                    fontWeight: "normal",
+                  }}
+              >
+                {t("simulation.confirm")}
+              </Button>
+              <Button
+                  variant="outlined"
+                  onClick={handleClose}
+                  sx={{
+                    margin: "0.5rem",
+                    fontSize: "0.875rem",
+                    fontWeight: "normal",
+                  }}
+              >
+                {t("simulation.cancel")}
+              </Button>
+            </Box>
+          </Box>
+        </Popover>
+      </>
   );
 }
 
